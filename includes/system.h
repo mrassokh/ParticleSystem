684e3e9e40a063c394cee84aa3c2d89737135326
Maksym Rassokha <mrassokh@e1r10p3.unit.ua>
2019-04-17 11:06:23 +0300

    test_includes

--- includes/system.h
+++ includes/system.h
@@ -1,5 +1,9 @@
 #include <vector>
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
+#include "SDL.h"
+#include <imgui.h>
+#include "glm/vec2.hpp"
+#include "GL/glew.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#define  WIN_SIZE {800, 600}
 void lalalala();
